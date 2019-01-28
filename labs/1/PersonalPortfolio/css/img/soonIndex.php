de87114034378337024aa77e9e702509a334ec20
kristian <kmidtgaard@csumb.edu>
2019-01-28 23:25:41 +0000

    save

---
+++ labs/1/PersonalPortfolio/css/img/soonIndex.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+         <meta charset="utf-8"/>
+         <title>Kristian Midtgard Personal Website</title>
+    </head>
+    <body>
+        <p>Hei</p>
+        <footer>
+            
+        </footer>
+    </body>
+       
+</html>
