7d839a01614fc04583c0947a12c7bc690f788d0e
kristian <kmidtgaard@csumb.edu>
2019-02-07 16:29:20 +0000

    save

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>hello</title>
+    </head>
+    <body>
+        <p>Hei</p>
+    </body>
+    <footer>
+        
+    </footer>
+</html>
